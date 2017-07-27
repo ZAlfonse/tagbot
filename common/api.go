a8bc93f0ec27a559bd0890558974c49c16d85e1c
Zak Alfonse <zalfonse@ruelala.com>
2017-07-27 11:22:22 -0400

    Update common

--- common/api.go
+++ common/api.go
@@ -1,10 +1,12 @@
 package common
 
+//Command is the object for issuing commands
 type Command struct {
 	Name string `json:"name"`
 	Args string `json:"args"`
 }
 
+//Response is the object for responding to commands
 type Response struct {
 	Command Command  `json:"command"`
 	Type    string   `json:"type"`
---
+++ common/nsq.go
@@ -0,0 +1,61 @@
+package common
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+// CommandHandler listens for commands on a given topic/channel
+type CommandHandler struct {
+	topic     string
+	channel   string
+	nsqConfig nsq.Config
+	consumer  nsq.Consumer
+}
+
+// HandleMessage handles nsq messages on it's topic/channel
+func (ch *CommandHandler) HandleMessage(m *nsq.Message) error {
+	fmt.Println(m.Body)
+	return nil
+}
+
+// Start listening for messages on your channel
+func (ch *CommandHandler) Start() {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	for {
+		select {
+		case <-ch.consumer.StopChan:
+			return
+		case <-sigChan:
+			ch.consumer.Stop()
+		}
+	}
+}
+
+func newCommandHandler(topic, channel string) *CommandHandler {
+	cfg := nsq.NewConfig()
+
+	consumer, err := nsq.NewConsumer(*topic, *channel, cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ch := CommandHandler{
+		topic,
+		channel,
+		cfg,
+		consumer,
+	}
+
+	consumer.AddHandler(&ch)
+	err = consumer.ConnectToNSQLookupd("nsqlookupd")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &ch
+}
